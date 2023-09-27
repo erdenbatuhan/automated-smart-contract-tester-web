5694def5462b9dc08b3b530306fba0d5778a01e9
Batuhan Erden <erden.batuhan@gmail.com>
2023-09-27 15:23:23 +0200

    add spinner component

---
+++ frontend/src/components/AppSpinner.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-if="spinner" class="overlay-container">
+    <!-- Overlay background -->
+    <div class="overlay-bg" />
+
+    <!-- Progress component -->
+    <v-progress-circular
+      class="overlay-item"
+      color="secondary"
+      indeterminate
+      :size="128"
+      :width="15"
+    />
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+
+export default {
+  name: 'AppAlert',
+  setup: () => {
+    const store = useStore();
+
+    return {
+      spinner: computed(() => store.getters['global/spinner'])
+    };
+  }
+};
+</script>
+
+<style scoped>
+@import "@/assets/styles/overlay.css";
+</style>
